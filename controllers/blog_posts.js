2b6653aee812102fd9763f0bb5dc41f0b1e78e7e
gramos <gerard.ramosm89@gmail.com>
2017-05-17 18:07:47 -0700

    changes

--- controllers/blog_posts.js
+++ controllers/blog_posts.js
@@ -6,7 +6,6 @@ module.exports = {
     res.send({ message: "Greeting from Blog API!" });
   },
   create(req, res) {
-    console.log('req.body is: ', req.body);
     console.log(Math.floor(new Date() / 1000));
     console.log('moment is: ', moment().format('YYYY/MM/DD HH:mma'));
     let newBlog = new Blog({
