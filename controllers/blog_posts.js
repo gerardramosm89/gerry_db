ad2091e619bd560f9ed07ade8991f09f9f6ab052
gramos <gerard.ramosm89@gmail.com>
2017-05-20 17:30:23 -0700

    changes

--- controllers/blog_posts.js
+++ controllers/blog_posts.js
@@ -2,6 +2,7 @@ const moment = require('moment');
 const Blog = require('../models/blog_posts');
 const { User } = require('../models/user');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
   greeting(req, res) {
@@ -22,21 +23,36 @@ module.exports = {
   //   });
   // },
   create(req, res) {
-    console.log("req.body.newblog is: ", req.body.newblog);
-    newBlogPost = new Blog(req.body.newblog);
-    newBlogPost.save();
-    User.findOneAndUpdate({ username: req.body.username}, {$push: {blogs: newBlogPost }})
-      .then(response => {
-        return res.send({ response });
-      });
+    jwt.verify(req.body.token, 'secret', function(err, decoded){
+      console.log('decoded is: ', decoded);
+      authorId = decoded.user[0]._id;
+      title = req.body.newblog.title;
+      content = req.body.newblog.content;
+      username = decoded.user[0].username;
+      console.log("req.body.newblog is: ", { authorId, title, content });
+      newBlogPost = new Blog({ authorId, title, content });
+      newBlogPost.save();
+      User.findOneAndUpdate({ username: username}, {$push: {blogs: newBlogPost }})
+        .then(response => {
+          return res.send({ response });
+        });
+    });
+
   },
   findPerUser(req, res) {
-    let author = req.body.username;
-    User.findOne({ username: req.body.username })
-      .populate('blogs')
-      .then(user => {
-        console.log(user.blogs);
-        res.send(user);
-      });
+    let author;
+    jwt.verify(req.body.token, 'secret', function(err, decoded){
+      console.log('decoded is: ', decoded);
+      author = decoded.user[0].username;
+      console.log('user that is saying hello is: ', decoded.user[0].username);
+      console.log('author is: ', author);
+      User.findOne({ username: author })
+        .populate('blogs')
+        .then(user => {
+          console.log(user.blogs);
+          res.send(user);
+        });
+    });
+
   }
 };
