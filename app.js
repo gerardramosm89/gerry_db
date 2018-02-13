3c12350a180904f5d3619208c67b3c60e2346fdd
gerardramosayasdi <gerard.ramosm89@gmail.com>
2018-02-13 15:37:42 -0800

    cleanup

--- app.js
+++ app.js
@@ -4,19 +4,12 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
 const routes = require('./routes/routes');
-const expressGraphQL = require('express-graphql');
-const schema = require('./GraphQLSchemas/schema');
 const keys = require('./config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/gerry_db');
 // mongoose.createConnection('mongodb://localhost:27017/gerry_db');
-
-app.use('/graphql', expressGraphQL({
-  schema,
-  graphiql: true
-}));
 
 app.use(cors());
 app.use(bodyParser.json({ limit: '5mb' }));
